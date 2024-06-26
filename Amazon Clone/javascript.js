a7257baea0c205f631bcad003cc3da9ecfc568b5
Yash-Raj20 <ratneshkumarstm987@gmail.com>
2024-06-26 21:30:22 +0530

    first commit

---
+++ Amazon Clone/javascript.js
@@ -0,0 +1,21 @@
+// let slideBtnleft = document.getElementById("slide-btn-left")
+// let slideBtnright = document.getElementById("slide-btn-right")
+// let imgitem = document.querySelectorAll("image-item")
+
+// console.log(imgitem.length-1)
+
+// let startslider = 0
+// let endslider = imgitem.length-1 *100
+// slideBtnleft.addEventListener("click",()=>{
+    
+// })
+// slideBtnright.addEventListener("click",()=>{
+//     if(startslider >= -endslider){
+//         startslider = startslider - 100;
+//     }
+  
+//     imgitem.forEach(Element =>{
+//         Element.style.transform = `translateX(${startslider}%)`;
+//     })
+    
+// })
